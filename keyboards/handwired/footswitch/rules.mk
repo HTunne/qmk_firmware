cf50e949beb9c64e27881fd182fb26c2229ced34
HTunne <h.tunnicliffe@protonmail.com>
2019-10-15 22:07:23 +0100

    Add options for smaller hex file.

--- keyboards/handwired/footswitch/rules.mk
+++ keyboards/handwired/footswitch/rules.mk
@@ -40,3 +40,4 @@ BLUETOOTH = AdafruitBLE     # Enable Bluetooth with the Adafruit Feather 32u4 Bl
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs (+400)
+EXTRAFLAGS += -flto
