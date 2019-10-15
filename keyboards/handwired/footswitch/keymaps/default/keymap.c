359d41ce9adf8e0ce548944bce0b6a24718f17b0
HTunne <h.tunnicliffe@protonmail.com>
2019-10-15 22:06:52 +0100

    Fix layout, remove trailing commas.

--- keyboards/handwired/footswitch/keymaps/default/keymap.c
+++ keyboards/handwired/footswitch/keymaps/default/keymap.c
@@ -23,7 +23,7 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT( /* Base */
-        KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK, KC_MEDIA_PLAY_PAUSE,
+        KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK, KC_MEDIA_PLAY_PAUSE
     ),
 };
 
