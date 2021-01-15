cb9795dd3392e27696da2f1f6f7bf004999f0f2b
HTunne <h.tunnicliffe@protonmail.com>
2021-01-15 17:09:23 +0000

    Swap num and arrow layer order.

--- keyboards/handwired/ergodox/keymaps/htunne/keymap.c
+++ keyboards/handwired/ergodox/keymaps/htunne/keymap.c
@@ -1,8 +1,8 @@
 #include QMK_KEYBOARD_H
 
 #define QWERT 0
-#define ARROW 1
-#define NUMP  2
+#define NUMP  1
+#define ARROW 2
 #define GAME  3
 #define RESET 4
 
@@ -17,23 +17,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                 KC_MUTE,    KC_MPLY,
                                                         KC_BSPC,    KC_LGUI,    TT(NUMP),   TT(ARROW),  KC_ENT,     KC_SPC
     ),
-	[ARROW] = LAYOUT_ergodox_handwired(
-        TO(RESET),  KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_TRNS,    KC_TRNS,    KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,
-        KC_TRNS,    KC_NO,      KC_MS_U,    KC_NO,      KC_NO,      KC_NO,      KC_TRNS,    KC_TRNS,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_F12,
-        KC_TRNS,    KC_MS_L,    KC_MS_D,    KC_MS_R,    KC_NO,      KC_NO,                              KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT,    KC_NO,      KC_NO,
-        KC_TRNS,    KC_ACL0,    KC_ACL1,    KC_ACL2,    KC_NO,      KC_NO,      KC_TRNS,    KC_TRNS,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_TRNS,
-        KC_TRNS,    KC_NO,      KC_TRNS,    KC_TRNS,    KC_TRNS,                                                    KC_TRNS,    KC_NO,      KC_NO,      KC_NO,      KC_TRNS,
-                                                                    KC_NO,      KC_NO,      KC_NO,      KC_NO,
-                                                                                KC_TRNS,    KC_TRNS,
-                                                        KC_BTN1,    KC_TRNS,    TO(GAME),   TO(QWERT),  KC_TRNS,    KC_BTN2
-    ),
 	[NUMP] = LAYOUT_ergodox_handwired(
         TO(RESET),  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
         KC_TRNS,    KC_EXLM,    KC_AT,      KC_LCBR,    KC_RCBR,    KC_QUES,    KC_TRNS,    KC_TRNS,    KC_UP,      KC_P7,      KC_P8,      KC_P9,      KC_PPLS,    KC_TRNS,
         KC_TRNS,    KC_HASH,    KC_DLR,     KC_LPRN,    KC_RPRN,    KC_GRV,                             KC_DOWN,    KC_P4,      KC_P5,      KC_P6,      KC_PMNS,    KC_PSLS,
         KC_TRNS,    KC_PERC,    KC_CIRC,    KC_LBRC,    KC_RBRC,    KC_TILD,    KC_TRNS,    KC_TRNS,    KC_AMPR,    KC_P1,      KC_P2,      KC_P3,      KC_PEQL,    KC_TRNS,
         KC_TRNS,    KC_NO,      KC_TRNS,    KC_TRNS,    KC_TRNS,                                                    KC_TRNS,    KC_PDOT,    KC_P0,      KC_PAST,    KC_TRNS,
-                                                                    KC_NO,      KC_NO,      KC_NO,      KC_NO,
+                                                                    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
+                                                                                KC_TRNS,    KC_TRNS,
+                                                        KC_TRNS,    KC_TRNS,    TO(GAME),   TO(QWERT),  KC_TRNS,    KC_TRNS
+    ),
+	[ARROW] = LAYOUT_ergodox_handwired(
+        TO(RESET),  KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_TRNS,    KC_TRNS,    KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,
+        KC_TRNS,    KC_NO,      KC_MS_U,    KC_NO,      KC_NO,      KC_NO,      KC_TRNS,    KC_TRNS,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_F12,
+        KC_TRNS,    KC_MS_L,    KC_MS_D,    KC_MS_R,    KC_NO,      KC_NO,                              KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT,    KC_NO,      KC_NO,
+        KC_TRNS,    KC_ACL0,    KC_ACL1,    KC_ACL2,    KC_NO,      KC_NO,      KC_TRNS,    KC_TRNS,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_TRNS,
+        KC_TRNS,    KC_NO,      KC_TRNS,    KC_TRNS,    KC_TRNS,                                                    KC_TRNS,    KC_NO,      KC_NO,      KC_NO,      KC_TRNS,
+                                                                    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
                                                                                 KC_TRNS,    KC_TRNS,
                                                         KC_BTN1,    KC_TRNS,    TO(QWERT),  TO(GAME),   KC_TRNS,    KC_BTN2
     ),
@@ -43,8 +43,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,     KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                               KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,
         KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_VOLD,    KC_LSFT,    KC_Z,       KC_X        KC_C,       KC_V,       KC_B,       KC_N,
         KC_LCTL,    KC_BSLS,    KC_LALT,    KC_LGUI,    KC_PSCR,                                                    KC_LCTL,    KC_LALT,    KC_LGUI,    KC_NUHS,    KC_RCTL,
-                                                                    KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END,
-                                                                                KC_MUTE,    KC_MPLY,
+                                                                    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
+                                                                                KC_TRNS,    KC_TRNS,
                                                         KC_SPC,     KC_ENT,     TT(QWERT),  TT(QWERT),  KC_ENT,     KC_SPC
     ),
 	[RESET] = LAYOUT_ergodox_handwired(
